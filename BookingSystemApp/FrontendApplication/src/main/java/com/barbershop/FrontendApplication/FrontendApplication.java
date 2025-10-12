8113d2ccbbaeccb053ec606c7ecdd5d71f25115c
Mateus Manhani <mateus.manhani.dias@gmail.com>
2025-10-12 17:58:22 +0100

    Created A separate module for the frontend application

---
+++ BookingSystemApp/FrontendApplication/src/main/java/com/barbershop/FrontendApplication/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.barbershop.FrontendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
